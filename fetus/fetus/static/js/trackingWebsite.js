51ab5bd3ba6c6f0b7512283877274297d5a881da
LAPTOP-4O94A7A3\Jks <a0937027831@gmail.com>
2023-08-21 16:45:50 +0800

    廣告

---
+++ fetus/fetus/static/js/trackingWebsite.js
@@ -0,0 +1,6 @@
+function startTrack(){
+  var s=document.createElement('script');s.async=1;
+  s.src='https://ad.popout.cc/pixel.php?aid=yVTVTj5Rg6&t='+Date.now();
+  var t=document.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);
+}
+startTrack();
