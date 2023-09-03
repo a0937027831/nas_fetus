7a619a86f6b3fa4682561a772b1a001f7744c791
DESKTOP-QMEC56B\CHOU <a0937027831@gmail.com>
2023-09-04 00:31:21 +0800

    更新about

--- fetus/fetus/static/js/about.js
+++ fetus/fetus/static/js/about.js
@@ -1,5 +1,25 @@
 const body = document.querySelector('body');
-
+const swiper = new Swiper(".mySwiper", {
+	autoplay: {
+		delay: 5000,
+	},
+	disableOnInteraction: false,
+	pauseOnMouseEnter: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		1080: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		1280: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	},
+});
 function changeObj(){
     if(detectMob()){
         body.classList.remove('active');
