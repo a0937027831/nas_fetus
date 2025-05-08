a6c526440c7f2a58018c444267fe2c921e9288bf
DESKTOP-QMEC56B\CHOU <a0937027831@gmail.com>
2025-05-08 23:33:08 +0800

    修改enter point script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py wait_for_db
+# 自動產生 migration（若無變動則跳過）
+python manage.py makemigrations --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module fetus.wsgi
