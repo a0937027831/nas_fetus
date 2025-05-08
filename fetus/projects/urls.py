684e75cd08b1bb0193632999278717d9ce45c643
DESKTOP-QMEC56B\CHOU <a0937027831@gmail.com>
2025-05-09 03:14:12 +0800

    url 解析修正

--- fetus/projects/urls.py
+++ fetus/projects/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('works/', views.works, name='works'),
     # 某個分類底下的作品清單
     path('works/<int:tag_id>/', views.works_list, name='works_list'),
-    # 單一作品詳情，用 slug 而非數字 ID
+    # 單一作品詳情，用 slug （改成 str，可接受中文）
     path(
-        'works/<int:tag_id>/<slug:project_slug>/',
+        'works/<int:tag_id>/<str:project_slug>/',
         views.works_detail,
         name='works_detail'
     ),
